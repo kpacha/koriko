003941cf55fa2c25f9f355360ccc85f914bea3fc
Kpacha <kpacha666@gmail.com>
2013-04-20 13:49:52 +0200

    fixing koriko app tests

--- Tests/KorikoApplicationTest.php
+++ Tests/KorikoApplicationTest.php
@@ -4,21 +4,32 @@ namespace Oridoki\Koriko\Tests;
 
 use Oridoki\Koriko\Tests\Dummy\App\DummyApplication;
 use Oridoki\Koriko\Command\DemoCommand;
+use Oridoki\Koriko\Tests\Dummy\Command\DummyCommand;
 
 class KorikoApplicationTest extends \PHPUnit_Framework_TestCase
 {
     protected $_subject;
 
-    public function setUp()
-    {
-        $this->_subject = new DummyApplication('test');
-    }
-
     public function testCommandScan()
     {
+        $this->_subject = new DummyApplication('test');
         $this->_subject->loadCommands();
         $command = new DemoCommand;
-        $command->setContainer($this->_subject);
-        $this->_subject['console']->get($command->getName());
+        $this->assertInstanceOf(
+                '\Oridoki\Koriko\Command\DemoCommand',
+                $this->_subject['console']->get($command->getName()));
+    }
+
+    public function testCustomFolderCommandScan()
+    {
+        $this->_subject = new DummyApplication('test', null, array(
+            'command.namespace' => '\\Oridoki\\Koriko\\Tests\\Dummy\\Command\\',
+            'command.folder' => '/Tests/Dummy/Command'
+        ));
+        $this->_subject->loadCommands();
+        $command = new DummyCommand;
+        $this->assertInstanceOf(
+                '\Oridoki\Koriko\Tests\Dummy\Command\DummyCommand',
+                $this->_subject['console']->get($command->getName()));
     }
 }
