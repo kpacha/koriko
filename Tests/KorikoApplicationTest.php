14bea433a900b062e053e8fc03a79b74359d5a70
Kpacha <kpacha666@gmail.com>
2013-04-20 14:00:42 +0200

    mocking the logger

--- Tests/KorikoApplicationTest.php
+++ Tests/KorikoApplicationTest.php
@@ -5,15 +5,17 @@ namespace Oridoki\Koriko\Tests;
 use Oridoki\Koriko\Tests\Dummy\App\DummyApplication;
 use Oridoki\Koriko\Command\DemoCommand;
 use Oridoki\Koriko\Tests\Dummy\Command\DummyCommand;
+use Oridoki\Koriko\Tests\Mocks;
 
 class KorikoApplicationTest extends \PHPUnit_Framework_TestCase
 {
+    use Mocks;
+
     protected $_subject;
 
     public function testCommandScan()
     {
-        $this->_subject = new DummyApplication('test');
-        $this->_subject->loadCommands();
+        $this->_subject = $this->_buildApp();
         $command = new DemoCommand;
         $this->assertInstanceOf(
                 '\Oridoki\Koriko\Command\DemoCommand',
@@ -22,14 +24,21 @@ class KorikoApplicationTest extends \PHPUnit_Framework_TestCase
 
     public function testCustomFolderCommandScan()
     {
-        $this->_subject = new DummyApplication('test', null, array(
+        $this->_subject = $this->_buildApp(array(
             'command.namespace' => '\\Oridoki\\Koriko\\Tests\\Dummy\\Command\\',
             'command.folder' => '/Tests/Dummy/Command'
         ));
-        $this->_subject->loadCommands();
         $command = new DummyCommand;
         $this->assertInstanceOf(
                 '\Oridoki\Koriko\Tests\Dummy\Command\DummyCommand',
                 $this->_subject['console']->get($command->getName()));
+    }
+
+    protected function _buildApp(array $values = array())
+    {
+        $subject = new DummyApplication('test', null, $values);
+        $subject['logger'] = $this->loggerMock();
+        $subject->loadCommands();
+        return $subject;
     }
 }
