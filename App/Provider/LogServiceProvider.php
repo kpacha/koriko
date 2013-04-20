ad37a7a7e446a1e84c559a90b82ec30eb9925d42
Kpacha <kpacha666@gmail.com>
2013-04-20 12:33:48 +0200

    adding a custom log service procider

---
+++ App/Provider/LogServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Oridoki\Koriko\App\Provider;
+
+use Cilex\ServiceProviderInterface;
+use Cilex\Application;
+use Cilex\Provider\MonologServiceProvider;
+
+class CommandServiceProvider extends MonologServiceProvider
+    implements ServiceProviderInterface
+{
+    const LOG_PATH = '/../logs/koriko.log';
+    const LOG_NAME = 'koriko';
+
+    /**
+     * Set up the default config path
+     * @param \Cilex\Application $app
+     */
+    public function register(Application $app)
+    {
+        if(!isset($app['monolog.name'])) {
+            $app['monolog.name'] = function() {
+                return self::LOG_NAME;
+            };
+        }
+
+        if(!isset($app['monolog.logfile'])) {
+            $app['monolog.logfile'] = function() {
+                return __DIR__ . self::LOG_PATH;
+            };
+        }
+
+        parent::register($app);
+    }
+}
